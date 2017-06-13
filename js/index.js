871f2eb2f3947003fd6f9389eda5a5b2fa60ad33
pkt07 <tripathi.prabhat1996@gmail.com>
2017-06-14 00:17:15 +0530

    make the ball collide and return from wall

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
 var canvas = document.getElementById("mycanvas");
 var ctx = canvas.getContext("2d");
+var ballRadius = 10;
 var x = canvas.width/2;
-var y = canvas.height;
+var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -16,8 +17,16 @@ function drawBall() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    
     x += dx;
     y += dy;
 }
 
-setInterval(draw,3);
+setInterval(draw, 10);
