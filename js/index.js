fda616db6378ca0b976dbe6b1786b515a0934938
pkt07 <tripathi.prabhat1996@gmail.com>
2017-06-14 14:18:12 +0530

    add score

--- js/index.js
+++ js/index.js
@@ -17,6 +17,7 @@ var brickHeight = 24;
 var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
+var score=0;
 
 var bricks = [];
 for(c=0; c<brickColumnCount; c++) {
@@ -89,18 +90,28 @@ function collisionDetection() {
                 if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
                     dy = -dy;
                     b.status = 0;
+                    score++;
+                    if(score == brickRowCount*brickColumnCount) {
+                        alert("YOU WIN, CONGRATULATIONS!");
+                        document.location.reload();
+                    }
                 }
             }
         }
     }
 }
-
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: "+score, 8, 20);
+}
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBricks();
     drawBall();
     drawPaddle();
+    drawScore();
     collisionDetection();
     
    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
